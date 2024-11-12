5e9405c9a2e27ab0eacb1e348a4f5736bfca22d8
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+import { fileURLToPath } from 'node:url'
+import { inspect } from 'node:util'
+import type { NuxtConfig } from '@nuxt/schema'
+import type { AppConfig } from '~/types'
+
+const buildConfig = () => {
+  console.info('Loading config…')
+  const {
+    NUXT_DEBUG: debug = 'false',
+    NUXT_FIREBASE_CLOUD_REGION: region = 'europe-west9',
+    NUXT_FIREBASE_CLOUD_MAX_INSTANCES: maxInstances = '3',
+  } = process.env
+  const config: AppConfig = {
+    debug: debug === 'true',
+    maxInstances: Number(maxInstances),
+    region,
+  }
+  if (config.debug) {
+    console.info('Loaded config:', inspect(config, { sorted: true }))
+  }
+  return config
+}
+
+const config = buildConfig()
+const { debug, maxInstances, region } = config
+
+const nuxtConfig: NuxtConfig = {
+  build: {
+    transpile: ['vuetify'],
+  },
+  buildDir: fileURLToPath(new URL('nuxt-build', import.meta.url)),
+  compatibilityDate: '2024-04-03',
+  debug,
+  devtools: { enabled: true },
+  logLevel: 'info',
+  modules: ['@nuxtjs/robots'],
+  nitro: {
+    firebase: {
+      gen: 2,
+      httpsOptions: {
+        maxInstances,
+        region,
+      },
+    },
+    output: {
+      dir: fileURLToPath(new URL('dist/nuxt', import.meta.url)),
+    },
+  },
+  srcDir: 'src',
+}
+
+export default defineNuxtConfig(nuxtConfig)
---
+++ src/pages/demos/hello.vue
@@ -0,0 +1,7 @@
+<template>
+  <PageHead subtitle="Hello Demo" />
+  <div>
+    <div>Hello demo</div>
+    <Hello />
+  </div>
+</template>
