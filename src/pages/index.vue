2f30694c8a9464b86a2de11f4946d5cdb33dad07
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute()
+</script>
+
+<template>
+  <v-card class="pa-6">
+    <h1>Demo</h1>
+    <h6>Current route: {{ route.path }}</h6>
+  </v-card>
+  <v-card class="pa-6">
+    <Hello />
+  </v-card>
+</template>
