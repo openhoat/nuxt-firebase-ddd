963415dc367c979d3ab27b8c054926971857404a
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const text = ref<string>()
+const hello = () => {
+  console.log('hello!')
+  text.value = text.value ? undefined : 'world!'
+}
+</script>
+
+<template>
+  <div>
+    <h4>
+      <button @click="hello()">Hello</button>
+      <p>{{ text }}</p>
+    </h4>
+  </div>
+</template>
