d4f368277b40d8cd5a753b99302ad8c76a24c184
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import PageHead from '~/components/PageHead.vue'
+</script>
+
+<template>
+  <PageHead />
+  <div>
+    <div>Demo sandbox</div>
+    <div>Choose a demo in the navigation menu.</div>
+  </div>
+</template>
