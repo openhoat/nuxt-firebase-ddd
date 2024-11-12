65f1a427279d2543fd0537fba4ff62d8484e8113
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/pages/demos/hello.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import PageHead from '~/components/PageHead.vue'
+</script>
+
+<template>
+  <PageHead subtitle="Hello Demo" />
+  <div>
+    <div>Hello demo</div>
+    <Hello />
+  </div>
+</template>
