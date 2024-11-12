489d1203220954ff92329df99bfa47838aebfc92
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/components/PageHead.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const {
+  subtitle,
+  title = 'Demo sandbox',
+  description = 'Demo project about nuxt and firebase functions',
+} = defineProps({
+  title: String,
+  subtitle: String,
+  description: String,
+})
+const fullTitle = subtitle ? `${title} - ${subtitle}` : title
+</script>
+
+<template>
+  <Head>
+    <Title>{{ fullTitle }}</Title>
+    <Meta name="description" :content="description" />
+  </Head>
+</template>
