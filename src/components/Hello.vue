eb275153623242b341e6280ab64f8d857b01b89a
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/components/Hello.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const text = ref<string>()
+const hello = () => {
+  console.log('hello!')
+  text.value = text.value ? undefined : 'world!'
+}
+</script>
+
+<template>
+  <v-card-text>
+    <button @click="hello()">Hello</button>
+    <p>{{ text }}</p>
+  </v-card-text>
+</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const route = useRoute()
+const drawer = ref(false)
+const switchDrawer = () => {
+  drawer.value = !drawer.value
+}
+</script>
+
+<template>
+  <v-app id="inspire">
+    <client-only>
+      <v-navigation-drawer v-model="drawer">
+        <v-list-item title="Navigation" subtitle="Menu"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item to="/" title="Home"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item title="demos"></v-list-item>
+        <v-list-item to="/hello" title="Hello"></v-list-item>
+        <v-list-item to="/counter" title="Counter"></v-list-item>
+      </v-navigation-drawer>
+    </client-only>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="switchDrawer()"></v-app-bar-nav-icon>
+      <v-app-bar-title> Application </v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-card-text> Current route: {{ route.path }} </v-card-text>
+      <v-card> <slot /></v-card>
+    </v-main>
+  </v-app>
+</template>
