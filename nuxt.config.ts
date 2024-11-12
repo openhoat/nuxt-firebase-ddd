946e65cb23ac573d337f7331799b242bea123718
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ nuxt.config.ts
@@ -0,0 +1,67 @@
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { inspect } from 'node:util'
+import type { NuxtConfig } from '@nuxt/schema'
+import type { AppConfig } from '~/types'
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
+
+const buildConfig = () => {
+  console.info('Loading config…')
+  const {
+    NUXT_DEBUG: debug = 'false',
+    NUXT_FIREBASE_CLOUD_REGION: region = 'europe-west9',
+    NUXT_FIREBASE_CLOUD_MAX_INSTANCES: maxInstances = '3',
+  } = process.env
+  const config: AppConfig = {
+    debug: debug === 'true',
+    maxInstances: Number(maxInstances),
+    region,
+  }
+  if (config.debug) {
+    console.info('Loaded config:', inspect(config, { sorted: true }))
+  }
+  return config
+}
+
+const config = buildConfig()
+const { debug, maxInstances, region } = config
+
+const nuxtConfig: NuxtConfig = {
+  buildDir: join(rootDir, 'nuxt-build'),
+  compatibilityDate: '2024-04-03',
+  css: ['@mdi/font/css/materialdesignicons.css'],
+  debug,
+  devtools: { enabled: true },
+  imports: { autoImport: false },
+  logLevel: 'info',
+  modules: ['@nuxtjs/robots'],
+  nitro: {
+    firebase: {
+      gen: 2,
+      httpsOptions: {
+        maxInstances,
+        region,
+      },
+    },
+    output: {
+      dir: join(rootDir, 'dist/nuxt'),
+    },
+  },
+  sourcemap: {
+    server: true,
+    client: true,
+  },
+  srcDir: 'src',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern',
+        },
+      },
+    },
+  },
+}
+
+export default defineNuxtConfig(nuxtConfig)
---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const text = ref<string>()
+const hello = () => {
+  console.log('hello!')
+  text.value = text.value ? undefined : 'world!'
+}
+</script>
+
+<template>
+  <div>
+    <button @click="hello()">Hello</button>
+    <div :v-if="text !== undefined">{{ text }}</div>
+  </div>
+</template>
