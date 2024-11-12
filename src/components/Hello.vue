f7c9bd0408b6a5749fbcf58ee32f4622c42cb6ed
Olivier Penhoat <openhoat@gmail.com>
2024-11-12 16:49:23 +0100

    Initial import

---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const hello = () => {
+  console.log('hello!')
+}
+</script>
+
+<template>
+  <div>
+    <h4>
+      <button @click="hello()">Hello</button>
+    </h4>
+  </div>
+</template>
