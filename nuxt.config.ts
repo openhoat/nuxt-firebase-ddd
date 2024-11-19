f6b9bfa4e08dff4530ded34b4787a3ae33379de6
Olivier Penhoat <openhoat@gmail.com>
2024-11-19 15:39:16 +0100

    Add ui powered by vuetify (#11)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
   nitro: {
     firebase: {
       gen: 2,
@@ -14,4 +28,11 @@ export default defineNuxtConfig({
     },
   },
   srcDir: 'src',
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
