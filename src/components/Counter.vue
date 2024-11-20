06a4d0be2e0ac7a75ba2c54265f17532d235e005
Olivier Penhoat <openhoat@gmail.com>
2024-11-20 08:36:31 +0100

    Add firestore simple counter api

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const count = ref<number>()
+const fetchCounter = async () => {
+  const counterData = await $fetch<{ value: number }>('/api/counter')
+  console.log('counterData:', counterData)
+  count.value = counterData.value
+}
+</script>
+
+<template>
+  <v-card-text>
+    <button @click="fetchCounter()">Counter</button>
+    <p :v-if="count">{{ count }}</p>
+  </v-card-text>
+</template>
