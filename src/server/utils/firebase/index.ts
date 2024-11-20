259532aa47b7ce56c385c9ddb3ddea586c08f8e2
Olivier Penhoat <openhoat@gmail.com>
2024-11-20 08:36:31 +0100

    Add firestore simple counter api

---
+++ src/server/utils/firebase/index.ts
@@ -0,0 +1,36 @@
+import firebaseAdmin from 'firebase-admin'
+import {
+  type App as FirebaseApp,
+  cert,
+  getApp,
+  getApps,
+} from 'firebase-admin/app'
+import { type Firestore, getFirestore } from 'firebase-admin/firestore'
+
+const initFirebaseInstance = (): FirebaseApp => {
+  if (getApps().length) {
+    return getApp()
+  }
+  const config = useRuntimeConfig()
+  const {
+    firebaseAdminServiceAccount: useServiceAccount,
+    firebaseAdminClientEmail: clientEmail,
+    firebaseAdminPrivateKey: privateKey,
+    firebaseAdminProjectId: projectId,
+  } = config
+  const firebaseConfig = useServiceAccount
+    ? {
+        credential: cert({ clientEmail, privateKey, projectId }),
+      }
+    : undefined
+  return firebaseAdmin.initializeApp(firebaseConfig)
+}
+
+export const initFirebaseAdmin = (): {
+  firebaseApp: FirebaseApp
+  firestore: Firestore
+} => {
+  const firebaseApp = initFirebaseInstance()
+  const firestore = getFirestore(firebaseApp)
+  return { firebaseApp, firestore }
+}
