fcc1a3f5abac52501da00a1b2c08dd147fde9f6b
Olivier Penhoat <openhoat@gmail.com>
2024-11-25 07:46:44 +0100

    Add simple hello api

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
 const text = ref<string>()
-const hello = () => {
-  console.log('hello!')
-  text.value = text.value ? undefined : 'world!'
+const hello = async () => {
+  if (text.value !== undefined) {
+    text.value = undefined
+    return
+  }
+  const helloData = await $fetch<{ hello: string }>('/api/hello')
+  console.log('helloData:', helloData)
+  text.value = helloData.hello
 }
 </script>
 
 <template>
   <v-container>
     <v-btn @click="hello()">Hello</v-btn>
-    <v-card-text :v-if="text">{{ text }}</v-card-text>
+    <v-card-text :v-if="text !== undefined">{{ text }}</v-card-text>
   </v-container>
 </template>
