0ca2010d645074fe33ef0319a455d2e1f90631a4
Olivier Penhoat <openhoat@gmail.com>
2024-11-26 19:35:28 +0100

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath } from 'node:url'
+import type { GitHubActionOptions } from '@estruyf/github-actions-reporter'
 import type { ConfigOptions } from '@nuxt/test-utils/playwright'
 import { defineConfig, devices } from '@playwright/test'
 
@@ -21,7 +22,14 @@ export default defineConfig<ConfigOptions>({
   ],
   reporter: [
     process.env.CI
-      ? ['dot']
+      ? [
+          '@estruyf/github-actions-reporter',
+          <GitHubActionOptions>{
+            title: 'Demo sandbox e2e tests report',
+            useDetails: true,
+            showError: true,
+          },
+        ]
       : ['html', { outputFolder: './dist/test-e2e-html-report' }],
   ],
   respectGitIgnore: true,
