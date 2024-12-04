9636ba115c55ef606f58aefcb29e071ea42fd34b
Olivier Penhoat <openhoat@gmail.com>
2024-12-04 18:14:46 +0100

    Add counter api with firestore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,22 @@ const buildConfig = () => {
     NUXT_DEBUG: debug = 'false',
     NUXT_FIREBASE_CLOUD_REGION: region = 'europe-west9',
     NUXT_FIREBASE_CLOUD_MAX_INSTANCES: maxInstances = '3',
+    NUXT_FIREBASE_PROJECT_ID: firebaseProjectId,
+    NUXT_FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL:
+      firebaseServiceAccountClientEmail,
+    NUXT_FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY: firebaseServiceAccountPrivateKey,
   } = process.env
   const config: AppConfig = {
     debug: debug === 'true',
     maxInstances: Number(maxInstances),
     region,
+    runtimeConfig: {
+      firebaseServiceAccountClientEmail,
+      firebaseServiceAccountPrivateKey,
+      public: {
+        firebaseProjectId,
+      },
+    },
   }
   if (config.debug) {
     console.info('Loaded config:', inspect(config, { sorted: true }))
@@ -26,7 +37,7 @@ const buildConfig = () => {
 }
 
 const config = buildConfig()
-const { debug, maxInstances, region } = config
+const { debug, maxInstances, region, runtimeConfig } = config
 
 const nuxtConfig: NuxtConfig = {
   build: {
@@ -63,6 +74,7 @@ const nuxtConfig: NuxtConfig = {
       dir: join(rootDir, 'dist/nuxt'),
     },
   },
+  runtimeConfig,
   srcDir: 'src',
   vite: {
     css: {
