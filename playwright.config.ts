b6c862b3dd97dad09bf68d77ffc1e8c32bced4e9
Olivier Penhoat <openhoat@gmail.com>
2024-12-08 08:45:55 +0100

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,64 +17,66 @@ const { PLAYWRIGHT_BASE_URL, PLAYWRIGHT_ENV } = process.env
 if (PLAYWRIGHT_ENV) {
   config({ path: `.env.${PLAYWRIGHT_ENV}` })
 }
-const startLocalServer = isCI && !PLAYWRIGHT_BASE_URL
-const baseURL = PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000'
-const projects = [
-  {
-    name: 'chromium',
-    use: devices['Desktop Chrome'],
-  },
-]
-const reporter: PlaywrightTestConfig['reporter'] = isCI
-  ? [
-      ['list'],
-      [
-        '@estruyf/github-actions-reporter',
-        <GitHubActionOptions>{
-          title: 'Demo sandbox e2e tests report',
-          useDetails: true,
-          showError: true,
-        },
-      ],
-    ]
-  : [['html', { outputFolder: './dist/test/e2e/html-report' }]]
-const retries = isCI ? 2 : 0
-const workers = isCI ? 1 : undefined
 
-console.log('baseURL')
-const webServer = startLocalServer
-  ? {
-      command: 'pnpm start',
-      url: baseURL,
-      reuseExistingServer: false,
-    }
-  : undefined
-
-export default defineConfig<ConfigOptions>({
-  forbidOnly: isCI,
-  outputDir: './dist/test/e2e/results',
-  projects,
-  reporter,
-  respectGitIgnore: true,
-  retries,
-  testDir: './test/e2e',
-  use: {
-    baseURL,
-    nuxt: {
-      build: false,
-      host: baseURL,
-      nuxtConfig: {
-        nitro: {
-          output: {
-            dir: fileURLToPath(new URL('dist/nuxt', import.meta.url)),
+const buildPwConfig = (): PlaywrightTestConfig<ConfigOptions> => {
+  const startLocalServer = isCI && !PLAYWRIGHT_BASE_URL
+  const baseURL = PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000'
+  const projects = [
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome'],
+    },
+  ]
+  const reporter: PlaywrightTestConfig['reporter'] = isCI
+    ? [
+        ['list'],
+        [
+          '@estruyf/github-actions-reporter',
+          <GitHubActionOptions>{
+            title: 'Demo sandbox e2e tests report',
+            useDetails: true,
+            showError: true,
+          },
+        ],
+      ]
+    : [['html', { outputFolder: './dist/test/e2e/html-report' }]]
+  const retries = isCI ? 2 : 0
+  const workers = isCI ? 1 : undefined
+  const webServer = startLocalServer
+    ? {
+        command: 'pnpm start',
+        url: baseURL,
+        reuseExistingServer: false,
+      }
+    : undefined
+  return {
+    forbidOnly: isCI,
+    outputDir: './dist/test/e2e/results',
+    projects,
+    reporter,
+    respectGitIgnore: true,
+    retries,
+    testDir: './test/e2e',
+    use: {
+      baseURL,
+      nuxt: {
+        build: false,
+        host: baseURL,
+        nuxtConfig: {
+          nitro: {
+            output: {
+              dir: fileURLToPath(new URL('dist/nuxt', import.meta.url)),
+            },
           },
         },
+        rootDir: fileURLToPath(new URL('.', import.meta.url)),
+        server: startLocalServer,
       },
-      rootDir: fileURLToPath(new URL('.', import.meta.url)),
-      server: startLocalServer,
+      trace: 'on-first-retry',
     },
-    trace: 'on-first-retry',
-  },
-  webServer,
-  workers,
-})
+    webServer,
+    workers,
+  }
+}
+
+export default defineConfig<ConfigOptions>(buildPwConfig())
