d4e098ca76e01b831afaf45e72647deb629495e8
Olivier Penhoat <openhoat@gmail.com>
2024-12-19 15:46:47 +0100

    Add firebase authentication

---
+++ src/domains/user/plugins/04-authorization-header.plugin.ts
@@ -0,0 +1,19 @@
+import { useAuthStore } from '../stores/auth-store'
+
+export default defineNuxtPlugin({
+  dependsOn: ['auth-store-plugin'],
+  name: 'user-jwt-plugin',
+  setup: () => {
+    const authStore = useAuthStore()
+    const withAuthorization = async (headers: Headers = new Headers()) => {
+      const { user } = authStore
+      if (!user) {
+        return undefined
+      }
+      const token = await user.getJwt()
+      headers.append('Authorization', `Bearer ${token}`)
+      return headers
+    }
+    return { provide: { withAuthorization } }
+  },
+})
