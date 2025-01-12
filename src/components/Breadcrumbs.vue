8d108480cac9cd4d86f19a2829c9c2354d3a8e2f
Olivier Penhoat <openhoat@gmail.com>
2025-01-12 12:41:18 +0100

    Add vuetify

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { buildBreadcrumbsItems } from '~/utils/breadcrumbs-helper'
+import { useRoute } from '#app'
+</script>
+
+<template>
+  <v-breadcrumbs icon="mdi-home" :items="buildBreadcrumbsItems(useRoute().path)" />
+</template>
